34fbd0856b60576895a0d20a59e85bc814b2861a
Ernesto Cruz <ernesto.cruzoviedo@gmail.com>
2020-09-09 18:57:57 -0500

    Validate if the header/footer files exist before to load them

--- nbgrader/tests/preprocessors/test_headerfooter.py
+++ nbgrader/tests/preprocessors/test_headerfooter.py
@@ -26,6 +26,22 @@ class TestIncludeHeaderFooter(BaseTestPreprocessor):
         orig_cells = orig_nb.cells[:]
         nb = preprocessor.preprocess(orig_nb, {})[0]
         assert nb.cells == (cells + orig_cells)
+    
+    def test_validate_if_header_exists(self, preprocessor):
+        """Does the prepocess check if the header file exists before load it?"""
+        preprocessor.header = os.path.join(os.path.dirname(__file__), "files", "nonexistent_header.ipynb")
+        
+        orig_nb = self._read_nb(os.path.join("files", "test.ipynb"))
+        nb = preprocessor.preprocess(orig_nb, {})[0]
+        assert nb.cells == orig_nb.cells[:]
+
+    def test_validate_if_footer_exists(self, preprocessor):
+        """Does the prepocess check if the footer file exists before load it?"""
+        preprocessor.footer = os.path.join(os.path.dirname(__file__), "files", "nonexistent_footer.ipynb")
+        
+        orig_nb = self._read_nb(os.path.join("files", "test.ipynb"))
+        nb = preprocessor.preprocess(orig_nb, {})[0]
+        assert nb.cells == orig_nb.cells[:]
 
     def test_concatenate_footer(self, preprocessor):
         """Is the footer appended correctly?"""
